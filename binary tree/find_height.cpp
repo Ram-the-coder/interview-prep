84af076157890e027c773fd93753a8efb138af01
unknown <ramvard@gmail.com>
2020-07-18 21:38:15 +0530

    binary tree: find height of tree

---
+++ binary tree/find_height.cpp
@@ -0,0 +1,28 @@
+/* Tree node structure  used in the program
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};*/
+
+void find_height(Node *root, int cur_ht, int &max_ht) {
+    max_ht = max(max_ht, cur_ht);
+    if(root->left) find_height(root->left, cur_ht+1, max_ht);
+    if(root->right) find_height(root->right, cur_ht+1, max_ht);
+}
+
+
+// return the Height of the given Binary Tree
+int height(Node* root)
+{
+   // Your code here
+   int max_ht = 0;
+   find_height(root, 1, max_ht);
+   return max_ht;
+}
